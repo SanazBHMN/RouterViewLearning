f6e02a002675b9dc15520b665623499909a69b98
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-12-03 15:05:22 +0330

    Use a button to show contact info

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,10 +1,18 @@
 <script setup>
-import { useRoute, RouterView } from "vue-router";
+import { useRoute, useRouter, RouterView } from "vue-router";
 import cars from "../data/cars.json";
 
 const route = useRoute();
+const router = useRouter();
 
-const car = cars.find((c) => c.id === parseInt(route.params.id));
+const carId = parseInt(route.params.id);
+
+const car = cars.find((c) => c.id === carId);
+
+const showContact = () => {
+  if (carId === 4) return;
+  router.push(`/cars/${carId}/contact`);
+};
 </script>
 
 <template>
@@ -13,6 +21,7 @@ const car = cars.find((c) => c.id === parseInt(route.params.id));
     <p>{{ car.name }}</p>
     <p>{{ car.year }}</p>
     <p>{{ car.price }}</p>
+    <button @click="showContact">Click for contact</button>
     <RouterView />
   </div>
 </template>
